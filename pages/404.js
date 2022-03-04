de56c8de35924131ab7d6f19c952d0615f7fd750
AyariiAsma <ayariasma14@gmail.com>
2022-03-04 12:25:39 +0100

    initial

---
+++ pages/404.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Head from 'next/head'
+
+const NotFoud = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [])
+
+    return ( 
+        <>
+    <Head>
+      <title>Ninja List | 404</title>
+      <meta name="keywords" content="ninjas"/>
+    </Head>
+        <div className="not-found">
+            <h1>Ooooops...</h1>
+            <h2>That page cannot be found.</h2>
+            <p>Go back to the <Link href="/">HomePage</Link> </p>
+        </div>
+        </>
+     );
+}
+ 
+export default NotFoud;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+          <title>Ninja List | About</title>
+          <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+            <h1>test</h1>
+            <p>Si nous partons de la définition de la nature dans le dictionnaire nous trouvons que c'est l'ensemble des éléments dans le monde physique qui ne sont pas transformés par l'être humain. Alors elle a une beauté originale qui n'est pas salie par l'homme.</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
